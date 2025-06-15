2410c222b5acfa83d46460953023202ef68009a3
VONSUNG <celestin@vonsung.co.rw>
2025-06-15 12:38:26 +0200

    Lab 4 updated

--- Labs - 4.py
+++ Labs - 4.py
@@ -198,3 +198,111 @@ knn1_pred = knn1.fit(X_train, y_train).predict(X_test)
 np.mean(y_test != knn1_pred), np.mean(y_test != "No")
 
 confusion_table(knn1_pred, y_test)
+
+
+# Poisson regression 
+
+Bike = load_data('Bikeshare')
+X = MS(['mnth',
+        'hr',
+        'workingday',
+        'temp',
+        'weathersit']).fit_transform(Bike)
+Y = Bike['bikers']
+M_lm = sm.OLS(Y, X).fit()
+summarize(M_lm)
+
+hr_encode = contrast('hr', 'sum')
+mnth_encode = contrast('mnth', 'sum')
+
+X2 = MS([mnth_encode,
+         hr_encode,
+        'workingday',
+        'temp',
+        'weathersit']).fit_transform(Bike)
+M2_lm = sm.OLS(Y, X2).fit()
+S2 = summarize(M2_lm)
+S2
+
+np.sum((M_lm.fittedvalues - M2_lm.fittedvalues)**2)
+np.allclose(M_lm.fittedvalues, M2_lm.fittedvalues)
+coef_month = S2[S2.index.str.contains('mnth')]['coef']
+coef_month
+
+months = Bike['mnth'].dtype.categories
+coef_month = pd.concat([
+                       coef_month,
+                       pd.Series([-coef_month.sum()],
+                                  index=['mnth[Dec]'
+                                 ])
+                       ])
+coef_month
+
+
+fig_month, ax_month = subplots(figsize=(8,8))
+x_month = np.arange(coef_month.shape[0])
+ax_month.plot(x_month, coef_month, marker='o', ms=10)
+ax_month.set_xticks(x_month)
+ax_month.set_xticklabels([l[5] for l in coef_month.index], fontsize=20)
+ax_month.set_xlabel('Month', fontsize=20)
+ax_month.set_ylabel('Coefficient', fontsize=20)
+
+
+coef_hr = S2[S2.index.str.contains('hr')]['coef']
+coef_hr = coef_hr.reindex(['hr[{0}]'.format(h) for h in range(23)])
+coef_hr = pd.concat([coef_hr,
+                     pd.Series([-coef_hr.sum()], index=['hr[23]'])
+                    ])
+
+
+fig_hr, ax_hr = subplots(figsize=(8,8))
+x_hr = np.arange(coef_hr.shape[0])
+ax_hr.plot(x_hr, coef_hr, marker='o', ms=10)
+ax_hr.set_xticks(x_hr[::2])
+ax_hr.set_xticklabels(range(24)[::2], fontsize=20)
+ax_hr.set_xlabel('Hour', fontsize=20)
+ax_hr.set_ylabel('Coefficient', fontsize=20)
+
+
+# Poisson regression
+M_pois = sm.GLM(Y, X2, family=sm.families.Poisson()).fit()
+S_pois = summarize(M_pois)
+coef_month = S_pois[S_pois.index.str.contains('mnth')]['coef']
+coef_month = pd.concat([coef_month,
+                        pd.Series([-coef_month.sum()],
+                                   index=['mnth[Dec]'])])
+coef_hr = S_pois[S_pois.index.str.contains('hr')]['coef']
+coef_hr = pd.concat([coef_hr,
+                     pd.Series([-coef_hr.sum()],
+                     index=['hr[23]'])])
+
+
+fig_month, ax_month = subplots(figsize=(8,8))
+x_month = np.arange(coef_month.shape[0])
+ax_month.plot(x_month, coef_month, marker='o', ms=10)
+ax_month.set_xticks(x_month)
+ax_month.set_xticklabels([l[5] for l in coef_month.index], fontsize=20)
+ax_month.set_xlabel('Month', fontsize=20)
+ax_month.set_ylabel('Coefficient', fontsize=20)
+
+
+fig_pois, (ax_month, ax_hr) = subplots(1, 2, figsize=(16,8))
+ax_month.plot(x_month, coef_month, marker='o', ms=10)
+ax_month.set_xticks(x_month)
+ax_month.set_xticklabels([l[5] for l in coef_month.index], fontsize=20)
+ax_month.set_xlabel('Month', fontsize=20)
+ax_month.set_ylabel('Coefficient', fontsize=20)
+ax_hr.plot(x_hr, coef_hr, marker='o', ms=10)
+ax_hr.set_xticklabels(range(24)[::2], fontsize=20)
+ax_hr.set_xlabel('Hour', fontsize=20)
+ax_hr.set_ylabel('Coefficient', fontsize=20)
+
+
+fig, ax = subplots(figsize=(8, 8))
+ax.scatter(M2_lm.fittedvalues,
+           M_pois.fittedvalues,
+           s=20)
+ax.set_xlabel('Linear Regression Fit', fontsize=20)
+ax.set_ylabel('Poisson Regression Fit', fontsize=20)
+ax.axline([0,0], c='black', linewidth=3,
+          linestyle='--', slope=1)
