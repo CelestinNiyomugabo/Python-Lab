d8a85c64eb950b2641ff3e758ae890437cc725b5
VONSUNG <celestin@vonsung.co.rw>
2025-06-22 12:42:19 +0200

    Lab 6 updated

---
+++ Labs - 6.py
@@ -0,0 +1,481 @@
+import numpy as np
+import pandas as pd
+from matplotlib.pyplot import subplots
+from statsmodels.api import OLS
+import sklearn.model_selection as skm
+import sklearn.linear_model as skl
+from sklearn.preprocessing import StandardScaler
+from ISLP import load_data
+from ISLP.models import ModelSpec as MS
+from functools import partial
+
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+from sklearn.cross_decomposition import PLSRegression
+from ISLP.models import \
+     (Stepwise,
+      sklearn_selected,
+      sklearn_selection_path)
+from l0bnb import fit_path
+
+import matplotlib.pyplot as plt
+
+# 1. Subset selection 
+# ====================================
+
+# a. Forward selection
+# ------------------------------------
+Hitters = load_data('Hitters')
+np.isnan(Hitters['Salary']).sum()
+Hitters = Hitters.dropna()
+Hitters.shape
+
+"""
+This score is not built in as a metric to sklearn. We therefore define a function to compute it ourselves, and use it as a scorer. 
+"""
+# Define the negative Cp statistic
+def nCp(sigma2, estimator, X, Y):
+    "Negative Cp statistic"
+    n, p = X.shape
+    Yhat = estimator.predict(X)
+    RSS = np.sum((Y - Yhat)**2)
+    return -(RSS + 2 * p * sigma2) / n 
+
+# Create a design matrix excluding the 'Salary' column
+design = MS(Hitters.columns.drop('Salary')).fit(Hitters)
+Y = np.array(Hitters['Salary'])
+X = design.transform(Hitters)
+sigma2 = OLS(Y,X).fit().scale
+
+# Define the negative Cp statistic as a partial function
+neg_Cp = partial(nCp, sigma2)
+strategy = Stepwise.first_peak(design,
+                               direction='forward',
+                               max_terms=len(design.terms))
+
+# Fit the model using the defined strategy
+hitters_MSE = sklearn_selected(OLS,
+                               strategy)
+hitters_MSE.fit(Hitters, Y)
+hitters_MSE.selected_state_
+
+# Using neg_Cp results in a smaller model, as expected, with just 10 variables selected.
+hitters_Cp = sklearn_selected(OLS,
+                               strategy,
+                               scoring=neg_Cp)
+hitters_Cp.fit(Hitters, Y)
+hitters_Cp.selected_state_
+
+
+# Choosing Among Models Using the Validation Set Approach and Cross-Validation
+strategy = Stepwise.fixed_steps(design,
+                                len(design.terms),
+                                direction='forward')
+full_path = sklearn_selection_path(OLS, strategy)
+
+full_path.fit(Hitters, Y)
+Yhat_in = full_path.predict(Hitters)
+Yhat_in.shape
+
+# Plotting the in-sample MSE for the forward stepwise selection
+mse_fig, ax = subplots(figsize=(10,6))
+insample_mse = ((Yhat_in - Y[:,None])**2).mean(0)
+n_steps = insample_mse.shape[0]
+ax.plot(np.arange(n_steps),
+        insample_mse,
+        'k', # color black
+        label='In-sample')
+ax.set_ylabel('MSE',
+              fontsize=20)
+ax.set_xlabel('# steps of forward stepwise',
+              fontsize=20)
+ax.set_xticks(np.arange(n_steps)[::2])
+ax.legend()
+ax.set_ylim([50000,250000])
+
+# Cross-validation
+# ------------------------------------
+K = 5
+kfold = skm.KFold(K,
+                  random_state=0,
+                  shuffle=True)
+Yhat_cv = skm.cross_val_predict(full_path,
+                                Hitters,
+                                Y,
+                                cv=kfold)
+Yhat_cv.shape
+
+# Calculate the mean squared error for each fold
+cv_mse = []
+for train_idx, test_idx in kfold.split(Y):
+    errors = (Yhat_cv[test_idx] - Y[test_idx,None])**2
+    cv_mse.append(errors.mean(0)) # column means
+cv_mse = np.array(cv_mse).T
+cv_mse.shape
+
+# Plotting the cross-validated MSE
+ax.errorbar(np.arange(n_steps), 
+            cv_mse.mean(1),
+            cv_mse.std(1) / np.sqrt(K),
+            label='Cross-validated',
+            c='r') # color red
+ax.set_ylim([50000,250000])
+ax.legend()
+mse_fig
+
+
+# Validation set approach
+validation = skm.ShuffleSplit(n_splits=1, 
+                              test_size=0.2,
+                              random_state=0)
+for train_idx, test_idx in validation.split(Y):
+    full_path.fit(Hitters.iloc[train_idx],
+                  Y[train_idx])
+    Yhat_val = full_path.predict(Hitters.iloc[test_idx])
+    errors = (Yhat_val - Y[test_idx,None])**2
+    validation_mse = errors.mean(0)
+
+# Plotting the validation set MSE
+ax.plot(np.arange(n_steps), 
+        validation_mse,
+        'b--', # color blue, broken line
+        label='Validation')
+ax.set_xticks(np.arange(n_steps)[::2])
+ax.set_ylim([50000,250000])
+ax.legend()
+mse_fig
+
+
+
+# Best Subset Selection
+# ------------------------------------
+D = design.fit_transform(Hitters)
+D = D.drop('intercept', axis=1)
+X = np.asarray(D)
+
+path = fit_path(X, 
+                Y,
+                max_nonzeros=X.shape[1])
+
+path[3]
+
+
+"""
+Ridge Regression and the Lasso
+====================================
+"""
+# Ridge Regression
+# ------------------------------------
+Xs = X - X.mean(0)[None,:]
+X_scale = X.std(0)
+Xs = Xs / X_scale[None,:]
+lambdas = 10**np.linspace(8, -2, 100) / Y.std()
+soln_array = skl.ElasticNet.path(Xs,
+                                 Y,
+                                 l1_ratio=0.,
+                                 alphas=lambdas)[1]
+soln_array.shape
+
+# Convert the solution array to a DataFrame for better readability
+soln_path = pd.DataFrame(soln_array.T,
+                         columns=D.columns,
+                         index=-np.log(lambdas))
+soln_path.index.name = 'negative log(lambda)'
+soln_path
+
+# Plotting the solution path for Ridge Regression
+path_fig, ax = subplots(figsize=(10,6))
+soln_path.plot(ax=ax, legend=False)
+ax.set_xlabel('$-\log(\lambda)$', fontsize=20)
+ax.set_ylabel('Standardized coefficients', fontsize=20)
+ax.legend(loc='upper left')
+
+# Extracting the coefficients for a specific lambda value
+beta_hat = soln_path.loc[soln_path.index[39]]
+lambdas[39], beta_hat
+
+# Calculating the L2 norm of the coefficients
+np.linalg.norm(beta_hat)
+
+
+
+# Lasso Regression
+# ------------------------------------
+beta_hat = soln_path.loc[soln_path.index[59]]
+lambdas[59], np.linalg.norm(beta_hat)
+
+# Fit a Ridge regression model with the selected lambda value
+ridge = skl.ElasticNet(alpha=lambdas[59], l1_ratio=0)
+scaler = StandardScaler(with_mean=True,  with_std=True)
+pipe = Pipeline(steps=[('scaler', scaler), ('ridge', ridge)])
+pipe.fit(X, Y)
+
+# Display the coefficients of the Ridge regression model
+np.linalg.norm(ridge.coef_)
+
+
+# Estimating Test Error of Ridge Regression
+# ------------------------------------
+validation = skm.ShuffleSplit(n_splits=1,
+                              test_size=0.5,
+                              random_state=0)
+ridge.alpha = 0.01
+results = skm.cross_validate(ridge,
+                             X,
+                             Y,
+                             scoring='neg_mean_squared_error',
+                             cv=validation)
+-results['test_score']
+
+
+# Hyperparameter Tuning for Ridge Regression
+param_grid = {'ridge__alpha': lambdas}
+grid = skm.GridSearchCV(pipe,
+                        param_grid,
+                        cv=validation,
+                        scoring='neg_mean_squared_error')
+grid.fit(X, Y)
+grid.best_params_['ridge__alpha']
+grid.best_estimator_
+
+
+# Hyperparameter Tuning with K-Fold Cross-Validation
+grid = skm.GridSearchCV(pipe, 
+                        param_grid,
+                        cv=kfold,
+                        scoring='neg_mean_squared_error')
+grid.fit(X, Y)
+grid.best_params_['ridge__alpha']
+grid.best_estimator_
+
+# Plotting the Cross-Validated MSE for Ridge Regression
+ridge_fig, ax = subplots(figsize=(10,6))
+ax.errorbar(-np.log(lambdas),
+            -grid.cv_results_['mean_test_score'],
+            yerr=grid.cv_results_['std_test_score'] / np.sqrt(K))
+ax.set_ylim([50000,250000])
+ax.set_xlabel('$-\log(\lambda)$', fontsize=20)
+ax.set_ylabel('Cross-validated MSE', fontsize=20)
+
+
+# Partial Least Squares Regression
+# ------------------------------------
+grid_r2 = skm.GridSearchCV(pipe, 
+                           param_grid,
+                           cv=kfold)
+grid_r2.fit(X, Y)
+
+
+# Extract the best alpha value for PLS regression
+r2_fig, ax = subplots(figsize=(10,6))
+ax.errorbar(-np.log(lambdas),
+            grid_r2.cv_results_['mean_test_score'],
+            yerr=grid_r2.cv_results_['std_test_score'] / np.sqrt(K))
+ax.set_xlabel('$-\log(\lambda)$', fontsize=20)
+ax.set_ylabel('Cross-validated $R^2$', fontsize=20)
+
+
+# Fast Cross-Validation for Solution Paths
+# ------------------------------------
+ridgeCV = skl.ElasticNetCV(alphas=lambdas, 
+                           l1_ratio=0,
+                           cv=kfold)
+pipeCV = Pipeline(steps=[('scaler', scaler),
+                         ('ridge', ridgeCV)])
+pipeCV.fit(X, Y)
+
+# Display the best alpha value from the cross-validated Ridge regression
+tuned_ridge = pipeCV.named_steps['ridge']
+ridgeCV_fig, ax = subplots(figsize=(10,6))
+ax.errorbar(-np.log(lambdas),
+            tuned_ridge.mse_path_.mean(1),
+            yerr=tuned_ridge.mse_path_.std(1) / np.sqrt(K))
+ax.axvline(-np.log(tuned_ridge.alpha_), c='k', ls='--')
+ax.set_ylim([50000,250000])
+ax.set_xlabel('$-\log(\lambda)$', fontsize=20)
+ax.set_ylabel('Cross-validated MSE', fontsize=20)
+
+# Display the best alpha value and the minimum mean squared error
+np.min(tuned_ridge.mse_path_.mean(1))
+
+# Display the coefficients of the tuned Ridge regression model
+tuned_ridge.coef_
+
+
+# Evaluating Test Error of Cross-Validated Ridge
+# -------------------------------------
+outer_valid = skm.ShuffleSplit(n_splits=1, 
+                               test_size=0.25,
+                               random_state=1)
+inner_cv = skm.KFold(n_splits=5,
+                     shuffle=True,
+                     random_state=2)
+ridgeCV = skl.ElasticNetCV(alphas=lambdas,
+                           l1_ratio=0,
+                           cv=inner_cv)
+pipeCV = Pipeline(steps=[('scaler', scaler),
+                         ('ridge', ridgeCV)])
+
+# Perform cross-validation to evaluate the test error of the cross-validated Ridge regression
+results = skm.cross_validate(pipeCV, 
+                             X,
+                             Y,
+                             cv=outer_valid,
+                             scoring='neg_mean_squared_error')
+-results['test_score']
+
+
+
+
+# The Lasso
+# =========================================
+lassoCV = skl.ElasticNetCV(n_alphas=100, 
+                           l1_ratio=1,
+                           cv=kfold)
+pipeCV = Pipeline(steps=[('scaler', scaler),
+                         ('lasso', lassoCV)])
+pipeCV.fit(X, Y)
+tuned_lasso = pipeCV.named_steps['lasso']
+tuned_lasso.alpha_
+
+# Plotting the Cross-Validated MSE for Lasso Regression
+lambdas, soln_array = skl.Lasso.path(Xs, 
+                                    Y,
+                                    l1_ratio=1,
+                                    n_alphas=100)[:2]
+soln_path = pd.DataFrame(soln_array.T,
+                         columns=D.columns,
+                         index=-np.log(lambdas))
+
+# Plotting the solution path for Lasso Regression
+path_fig, ax = subplots(figsize=(10,6))
+soln_path.plot(ax=ax, legend=False)
+ax.legend(loc='upper left')
+ax.set_xlabel('$-\log(\lambda)$', fontsize=20)
+ax.set_ylabel('Standardized coefficiients', fontsize=20)
+
+
+# Extracting the coefficients for a specific lambda value
+np.min(tuned_lasso.mse_path_.mean(1))
+
+# Plotting the Cross-Validated MSE for Lasso Regression
+lassoCV_fig, ax = subplots(figsize=(10,6))
+ax.errorbar(-np.log(tuned_lasso.alphas_),
+            tuned_lasso.mse_path_.mean(1),
+            yerr=tuned_lasso.mse_path_.std(1) / np.sqrt(K))
+ax.axvline(-np.log(tuned_lasso.alpha_), c='k', ls='--')
+ax.set_ylim([50000,250000])
+ax.set_xlabel('$-\log(\lambda)$', fontsize=20)
+ax.set_ylabel('Cross-validated MSE', fontsize=20)
+
+# Display the coefficients of the tuned Lasso regression model
+tuned_lasso.coef_
+
+
+
+"""
+PCR and PLS Regression
+=========================================
+"""
+
+# Principal Components Regression
+# =========================================
+
+pca = PCA(n_components=2)
+pca = PCA()
+linreg = skl.LinearRegression()
+pipe = Pipeline([('pca', pca),
+                 ('linreg', linreg)])
+pipe.fit(X, Y)
+pipe.named_steps['linreg'].coef_
+
+
+# Standardizing the data before PCA
+pipe = Pipeline([('scaler', scaler), 
+                 ('pca', pca),
+                 ('linreg', linreg)])
+pipe.fit(X, Y)
+pipe.named_steps['linreg'].coef_
+
+# Cross-validation using K-Fold
+param_grid = {'pca__n_components': range(1, 20)}
+grid = skm.GridSearchCV(pipe,
+                        param_grid,
+                        cv=kfold,
+                        scoring='neg_mean_squared_error')
+grid.fit(X, Y)
+
+# Display the best number of components and the best estimator
+pcr_fig, ax = subplots(figsize=(10,6))
+n_comp = param_grid['pca__n_components']
+ax.errorbar(n_comp,
+            -grid.cv_results_['mean_test_score'],
+            grid.cv_results_['std_test_score'] / np.sqrt(K))
+ax.set_ylabel('Cross-validated MSE', fontsize=20)
+ax.set_xlabel('# principal components', fontsize=20)
+ax.set_xticks(n_comp[::2])
+ax.set_ylim([50000,250000])
+
+# Display the best number of components and the best estimator
+Xn = np.zeros((X.shape[0], 1))
+cv_null = skm.cross_validate(linreg,
+                             Xn,
+                             Y,
+                             cv=kfold,
+                             scoring='neg_mean_squared_error')
+-cv_null['test_score'].mean()
+
+# Display the explained variance ratio of the PCA components
+pipe.named_steps['pca'].explained_variance_ratio_
+
+
+# Scree Plot: Explained Variance Ratio
+explained_var = pipe.named_steps['pca'].explained_variance_ratio_
+plt.figure(figsize=(8, 5))
+plt.plot(range(1, len(explained_var)+1), explained_var, marker='o', linestyle='-')
+plt.title('Scree Plot')
+plt.xlabel('Principal Component')
+plt.ylabel('Explained Variance Ratio')
+plt.xticks(range(1, len(explained_var)+1))
+plt.grid(True)
+plt.show()
+
+
+cumulative_var = np.cumsum(explained_var)
+
+plt.figure(figsize=(8, 5))
+plt.plot(range(1, len(cumulative_var)+1), cumulative_var, marker='o', linestyle='-')
+plt.axhline(y=0.95, color='r', linestyle='--', label='95% Variance')
+plt.title('Cumulative Explained Variance')
+plt.xlabel('Number of Components')
+plt.ylabel('Cumulative Variance Ratio')
+plt.xticks(range(1, len(cumulative_var)+1))
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+# Partial Least Squares Regression
+# =========================================
+pls = PLSRegression(n_components=2, 
+                    scale=True)
+pls.fit(X, Y)
+
+# Display the coefficients of the PLS regression model
+param_grid = {'n_components':range(1, 20)}
+grid = skm.GridSearchCV(pls,
+                        param_grid,
+                        cv=kfold,
+                        scoring='neg_mean_squared_error')
+grid.fit(X, Y)
+
+# Plotting the Cross-Validated MSE for PLS Regression
+pls_fig, ax = subplots(figsize=(10,6))
+n_comp = param_grid['n_components']
+ax.errorbar(n_comp,
+            -grid.cv_results_['mean_test_score'],
+            grid.cv_results_['std_test_score'] / np.sqrt(K))
+ax.set_ylabel('Cross-validated MSE', fontsize=20)
+ax.set_xlabel('# principal components', fontsize=20)
+ax.set_xticks(n_comp[::2])
+ax.set_ylim([50000,250000])
